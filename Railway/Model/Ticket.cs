1ba2ca8e39e0ab585bd6dfa83771894dadb15042
CSSDF018081 <CSSDF018081@PL18>
2023-09-02 07:47:21 +0800

    Add project files.

---
+++ Railway/Model/Ticket.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Railway.Model
+{
+    public class Ticket
+    {
+        private int Id { get; set; }
+        private int Category { get; set; }
+        private string Type { get; set; }
+        private int RouteId { get; set; }
+        private DateTime ExpirationDate { get; set; }
+    }
+}
---
+++ Railway/TicketValidation.cs
@@ -0,0 +1,25 @@
+﻿using Railway.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Railway
+{
+    public class TicketValidation
+    {
+       
+
+        TicketValidation()
+        {
+            
+        }
+
+        public void Validate(Ticket ticket, Station station)
+        {
+
+        }
+    }
+    
+}
